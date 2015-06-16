8ec5280c7014ce39f2e1c17b1b8775076da39327
tmvdh <TomV@nderheijden.be>
2015-06-16 12:31:25 +0200

    Noon

--- js/js.js
+++ js/js.js
@@ -13,21 +13,35 @@ $(document).ready(function(){
 	$('.datePicker').datepicker();
 	
 	
+	// Date submit - Return films
 	$('#dateSubmit').on("click", function(){
 		console.log('Click!');
 		$date = $('#date').val();
 		console.log($date);
 		$.getJSON("ajax_json_films.php?date=" + $date, function(filmdata){
 			console.log("JSON!");
+			$('#filmlist').empty();
 			$.each(filmdata, function(data){
-				$('#filmlist').append($('<li></li>').val(this.Film_ID).text(this.Title));
+				$('#filmlist').append($('<li></li>').val(this.Film_ID).text(this.Title).addClass('filmchoice'));
 			});
-			
+			//$('.filmchoice').on("click", function(){alert("It's alive!!!")});
+			if(!($('#filmlist').children().length > 0)){
+				$('#filmlist').append('<p>').text("Geen films voor deze datum");
+			} 
 		});
-	
-	
-	
-	});		  
-	
+	 });
+	 
+	 // Select film - Return showings
+	 $('li').on("click", function(){
+	 	$value = $(this).val();
+	 	console.log("filmclick! Film_ID:");
+	 	console.log($value);
+	 });
+	 
 	  	
+	 	
+	 
+	 		  
+	
+// End document.ready	  	
 });
